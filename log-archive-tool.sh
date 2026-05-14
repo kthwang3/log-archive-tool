f610bcfeff6eebc1854ddf079ec0747baad17b5d
kthwang3 <kthwang3@gmail.com>
2026-05-13 17:18:30 -0700

    completed

--- log-archive-tool.sh
+++ log-archive-tool.sh
@@ -2,6 +2,10 @@
 
 if [ -z "$1" ]; then
 	echo "Error, please enter directory again."
-	exit
-else
+	exit 1
+fi
+FILENAME=$(date +"logs_archive_%Y%m%d_%H%M%S").tar.gz
+mkdir -p log_archives
+tar -czf log_archives/$FILENAME $1
+echo "Archive Created: " $(date +"%Y-%m-%d %H:%M:%S") >> archive_log.txt
 	
